320544d9aa178e37b623ec94647efdf9523c98da
Espen <espen96@gmail.com>
2023-01-23 19:15:23 +0100

    update

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS= --opt-channelslast --listen --opt-split-attention --skip-install
+set COMMANDLINE_ARGS= --opt-channelslast --listen --opt-split-attention --skip-install --xformers
 
 call webui.bat
